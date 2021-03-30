77c5082b43547596859e78d1d24e5387a85bd8e1
Michal Sorát <michalsorat@gmail.com>
2021-03-31 01:37:32 +0200

    create note Cancel btn functionality

--- app/src/main/java/com/fiit/notes/CreateNote.kt
+++ app/src/main/java/com/fiit/notes/CreateNote.kt
@@ -1,12 +1,21 @@
 package com.fiit.notes
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class CreateNote: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addnote)
+
+        val cancelBtn = findViewById<Button>(R.id.cancelBtn)
+        cancelBtn.setOnClickListener{
+            val goToHomepage = Intent(this, Homepage:: class.java)
+            startActivity(goToHomepage)
+        }
+
     }
 
 }
