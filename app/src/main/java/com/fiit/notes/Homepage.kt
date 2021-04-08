6a7934cbd9b04b4cfdef837dd087ec0b581a1435
unknown <tankovic.lukas@yahoo.com>
2021-04-08 14:08:51 +0200

    [Tankovic] Uprava homepage

--- app/src/main/java/com/fiit/notes/Homepage.kt
+++ app/src/main/java/com/fiit/notes/Homepage.kt
@@ -2,15 +2,16 @@ package com.fiit.notes
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONException
 import org.json.JSONObject
+import org.w3c.dom.Text
 
 
 class Homepage : AppCompatActivity() {
@@ -32,28 +33,9 @@ class Homepage : AppCompatActivity() {
             createNote.putExtra("userID",userID)
             startActivity(createNote)
         }
-        val cards = findViewById<androidx.cardview.widget.CardView>(R.id.note_card)
-        val url = "http://10.0.2.2:8080/api/v1/notes/$userID"
-        //val jsonResponses: MutableList<String> = ArrayList()
-//
-        //val requestQueue = Volley.newRequestQueue(this)
-        //val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener<JSONObject?> {
-        //    response -> fun onResponse(response: JSONObject) {
-        //        try {
-        //            val jsonArray = response.getJSONArray("data")
-        //            for (i in 0 until jsonArray.length()) {
-        //                val jsonObject = jsonArray.getJSONObject(i)
-        //                val note = jsonObject.getString("note_name")
-        //                jsonResponses.add(note)
-        //            }
-        //        } catch (e: JSONException) {
-        //            e.printStackTrace()
-        //            Toast.makeText(this, "Error Occured", Toast.LENGTH_SHORT).show()
-        //        }
-        //    }
-        //}, Response.ErrorListener { error -> Toast.makeText(this, "Error occured", Toast.LENGTH_SHORT).show() })
-//
-        //requestQueue.add(jsonObjectRequest)
+        val noteList = findViewById<ListView>(R.id.note_card)
 
+        val url = "http://10.0.2.2:8080/api/v1/notes/$userID"
+        val jsonResponses: ArrayList<String> = ArrayList()
     }
 }
