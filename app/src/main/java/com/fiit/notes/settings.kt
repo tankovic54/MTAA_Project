6ea2d4df95ebb2e08a3642dc7dd4a8f33d68fff2
unknown <tankovic.lukas@yahoo.com>
2021-04-09 08:59:40 +0200

    [Tankovic] Settings changes
    Pridany button Delete All Notes a pridana funkcionalita Delete Account

--- app/src/main/java/com/fiit/notes/settings.kt
+++ app/src/main/java/com/fiit/notes/settings.kt
@@ -5,16 +5,18 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.github.dhaval2404.imagepicker.ImagePicker
 
 class settings : AppCompatActivity() {
     lateinit var displayName: TextView
     lateinit var profilePic: ImageView
     lateinit var signout: Button
+    lateinit var deletenotes : Button
     lateinit var deleteacc: Button
     lateinit var changePass: Button
     lateinit var changeEmail: Button
@@ -60,12 +62,38 @@ class settings : AppCompatActivity() {
 
         signout = findViewById<Button>(R.id.sign_out)
         signout.setOnClickListener {
+            Toast.makeText(this, "See you later alligator", Toast.LENGTH_SHORT).show()
             val goLog = Intent(this, Login::class.java)
             startActivity(goLog)
         }
+
+
+        deletenotes = findViewById(R.id.delete_notes)
+        deletenotes.setOnClickListener {
+            val url = "http://10.0.2.2:8080/api/v1/notes/$userID"
+            val queue = Volley.newRequestQueue(this)
+            val deleteRequest = StringRequest(Request.Method.DELETE, url,
+                    Response.Listener<String> { response ->
+                        Toast.makeText(this, "All notes deleted", Toast.LENGTH_SHORT).show()
+                    },
+                    Response.ErrorListener { Toast.makeText(this, "Error occured", Toast.LENGTH_SHORT).show() })
+            queue.add(deleteRequest)
+            val goHome = Intent(this, Homepage::class.java)
+            startActivity(goHome)
+        }
+
         deleteacc = findViewById(R.id.delete_acc)
         deleteacc.setOnClickListener {
+            val url = "http://10.0.2.2:8080/api/v1/users/$userID"
+            val queue = Volley.newRequestQueue(this)
+            val deleteRequest = StringRequest(Request.Method.DELETE, url,
+                    Response.Listener<String> { response ->
+                        Toast.makeText(this, "Account deleted", Toast.LENGTH_SHORT).show()
+                    },
+                    Response.ErrorListener { Toast.makeText(this, "Error occured", Toast.LENGTH_SHORT).show() })
+            queue.add(deleteRequest)
             val goLog = Intent(this, Login::class.java)
+            goLog.putExtra("userID",userID)
             startActivity(goLog)
         }
 
