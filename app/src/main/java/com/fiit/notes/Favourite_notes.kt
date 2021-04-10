d2c05c901b82658cac8bb209c2ccd2ca8e1380ad
unknown <tankovic.lukas@yahoo.com>
2021-04-10 13:13:07 +0200

    [Tankovic] Favourite Notes, Uprava UI
    Pridana obrazovka a button na zobrazenie oblubenych poznamok
    Upravene GUI tak, aby bolo konzistentne
    Odstraneny button Forgot password

--- app/src/main/java/com/fiit/notes/Favourite_notes.kt
+++ app/src/main/java/com/fiit/notes/Favourite_notes.kt
@@ -1,45 +1,32 @@
 package com.fiit.notes
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONArray
 import org.json.JSONObject
 
-
-class Homepage : AppCompatActivity() {
+class Favourite_notes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val bundle = intent.extras
         val userID = bundle!!.getString("userID")
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_homepage)
+        setContentView(R.layout.activity_favourite_notes)
 
-        val buttonBack = findViewById<Button>(R.id.homeBackButton)
-        buttonBack.setOnClickListener{
-            val goToLogin = Intent(this, Login:: class.java)
-            startActivity(goToLogin)
+        val backBtn = findViewById<Button>(R.id.favHomeButton)
+        backBtn.setOnClickListener {
+            val home = Intent(this, Homepage:: class.java)
+            home.putExtra("userID",userID)
+            startActivity(home)
         }
-        val addNoteBtn = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.addNote)
-        addNoteBtn.setOnClickListener{
-            val createNote = Intent(this, CreateNote:: class.java)
-            createNote.putExtra("userID",userID)
-            startActivity(createNote)
-        }
-        val settingBtn = findViewById<Button>(R.id.settings)
-        settingBtn.setOnClickListener {
-            val settingScreen = Intent(this, settings:: class.java)
-            settingScreen.putExtra("userID",userID)
-            startActivity(settingScreen)
-        }
-        val noteList = findViewById<ListView>(R.id.note_card)
+        val noteList = findViewById<ListView>(R.id.fav_notes_list)
         val list: MutableList<String> = ArrayList()
         val url = "http://10.0.2.2:8080/api/v1/notes/user/$userID"
         val queue = Volley.newRequestQueue(this)
@@ -51,12 +38,15 @@ class Homepage : AppCompatActivity() {
                     if (obj.length() > 0){
                         for (index: Int in 0..pocetPoznamok){
                             val obj1 = JSONObject(obj[index].toString())
+                            val favObj =  obj1["favourite"].toString()
                             val noteName = obj1["note"].toString()
-                            list.add(noteName)
+                            if (favObj == "true"){
+                                list.add(noteName)
+                            }
                         }
                     }
-                    else{
-                        val emptyNotes = "Add some notes"
+                    if (list.isEmpty()){
+                        val emptyNotes = "No favourite notes"
                         list.add(emptyNotes)
                     }
                     val arrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list)
@@ -65,18 +55,5 @@ class Homepage : AppCompatActivity() {
                 { Toast.makeText(this, "Error occured", Toast.LENGTH_SHORT).show() })
 
         queue.add(stringRequest)
-
-        val favButton = findViewById<Button>(R.id.home_fav_button)
-        favButton.setOnClickListener {
-            val favNotes = Intent(this, Favourite_notes:: class.java)
-            favNotes.putExtra("userID",userID)
-            startActivity(favNotes)
-        }
-        //noteList.setOnItemClickListener {
-        //    val editNote = Intent(this, CreateNote:: class.java)
-        //    editNote.putExtra("userID",userID)
-        //    startActivity(editNote)
-        //}
-
     }
 }
